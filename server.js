6b5cddbba1707d513ab29875c22559ccb3250411
Kai Mareving <karska.joanna@gmail.com>
2021-12-15 15:36:18 +0100

    28.2 First part [DONE]

--- server.js
+++ server.js
@@ -3,29 +3,18 @@ const path = require('path');
 
 const app = express();
 
-app.use((req, res, next) => {
-  res.show = (name) => {
-    res.sendFile(path.join(__dirname, `/client/${name}`));
-  };
-  next();
-});
+const messages = [];
 
-app.get('/style.css', (req, res) => {
-  res.show('style.css');
-});
+app.use(express.static(path.join(__dirname, '/client')));
 
-app.get('/app.js', (req, res) => {
-  res.show('app.js');
-});
-
-app.get('/', (req, res) => {
-  res.show('index.html');
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/index.html'));
 });
 
 app.use((req, res) => {
   res.status(404).send('404 not found...');
 });
 
-app.listen(8000, () => {
+app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running on port: 8000');
 });
